0f04b9446359504745e310bda72b724b23dbf353
19khadij <khad191919@gmail.com>
2025-02-11 05:06:25 +0500

    Solved one question Sparse arrays medium

---
+++ DataStructure&Algorithm/sparseArray.py
@@ -0,0 +1,26 @@
+
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    frequency={}
+    for s in stringList:
+        frequency[s]=frequency.get(s,0)+1
+        
+    result= [frequency.get(q,0) for q in queries]
+    return result
+if __name__ == '__main__':
+
+    stringList = ['abbb','baba','abc','abc','a','ab']
+    queries=['ab','a','abc']
+
+    res = matchingStrings(stringList, queries)
+
+    print(res)
